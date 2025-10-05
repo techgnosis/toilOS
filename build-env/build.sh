6928132082af856b359aa2f0ae6261a33ba73322
James Musselwhite <james.musselwhite@gmail.com>
2025-10-05 00:18:08 -0700

    no more buildenv for serious

--- build-env/build.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-podman build -t musl-builder:1 .
--- build-env/shell.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-podman run \
--it \
---rm \
-musl-builder:1 \
-sh
