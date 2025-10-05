4c48d1d5c751ca874e4dbb91a3aeb10f11b4a794
James Musselwhite <james.musselwhite@gmail.com>
2025-10-05 00:17:41 -0700

    still trying

--- youki/build.sh
+++ youki/build.sh
@@ -5,15 +5,12 @@ musl-dev \
 linux-headers \
 curl \
 gcc \
-libseccomp-dev \
-libseccomp-static
-
+libseccomp-dev
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.85.0
 
 source ~/.cargo/env
 
+cargo build --release --target aarch64-unknown-linux-musl --no-default-features --features v2
 
-rustup target add x86_64-unknown-linux-musl
-
-cargo build --release --target x86_64-unknown-linux-musl --no-default-features --features v2
+#cargo build --release --target x86_64-unknown-linux-musl --no-default-features --features v2
