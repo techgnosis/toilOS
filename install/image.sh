d01f9d1d41408b2741e82b9a53565652ef19d6bc
James Musselwhite <james.musselwhite@gmail.com>
2025-10-07 22:32:27 -0700

    second draft

--- install/image.sh
+++ install/image.sh
@@ -2,22 +2,29 @@
 
 set -euo pipefail
 
-dd if=/dev/zero of=disk.raw bs=1M count=8192
+echo "Creating raw disk image disk.raw"
+dd if=/dev/zero of=disk.raw bs=1M count=8192 &> image.log
 
-losetup /dev/loop0 disk.raw
+echo "Attaching /dev/loop0 to disk.raw"
+losetup /dev/loop0 disk.raw &> image.log
 
-sfdisk /dev/loop0 << EOF
+echo "Creating partition table"
+sfdisk /dev/loop0 &> image.log << EOF
 label: gpt
 start=2048, size=2097152, type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B, name="EFI System Partition"
 start=2099200, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, name="Linux Root"
 EOF
 
-losetup -d /dev/loop0
-modprobe -r loop
-modprobe loop
-losetup -P /dev/loop0 disk.raw
+echo "Restart loopback to create partition nodes"
+losetup -d /dev/loop0 &> image.log
+modprobe -r loop &> image.log
+modprobe loop &> image.log
+losetup -P /dev/loop0 disk.raw &> image.log
 
-mkfs.fat  /dev/loop0p1
-mkfs.ext4 /dev/loop0p2
+echo "Formatting ESP"
+mkfs.fat  /dev/loop0p1 &> image.log
+echo "Formatting root"
+mkfs.ext4 /dev/loop0p2 &> image.log
 
-losetup -d /dev/loop0
+echo "Detaching loopback from disk.raw"
+losetup -d /dev/loop0 &> image.log
