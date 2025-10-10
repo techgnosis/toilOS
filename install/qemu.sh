c406e14ba297775f5fef519e5f039439f7451976
James Musselwhite <james.musselwhite@gmail.com>
2025-10-09 17:55:51 -0700

    qemu at least runs

--- install/qemu.sh
+++ install/qemu.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 qemu-system-x86_64 \
 -enable-kvm \
 -m 16G \
--drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
--drive if=pflash,format=raw,file=OVMF_VARS.fd \
+-drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/ovmf/OVMF_CODE.fd \
+-drive if=pflash,format=raw,file=./OVMF_VARS.fd \
 -drive file=disk.raw,format=raw
