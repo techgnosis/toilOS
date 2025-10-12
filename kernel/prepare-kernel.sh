3dc938f777c48da1d710916b7eb3e1d889f3720a
James Musselwhite <james.musselwhite@gmail.com>
2025-10-12 12:35:56 -0700

    cmdline injection works

--- kernel/prepare-kernel.sh
+++ kernel/prepare-kernel.sh
@@ -16,6 +16,7 @@ objcopy \
 --add-section .linux="bzImage" \
 --change-section-vma .linux=0x1000000 \
 --set-section-flags .linux=load \
+--add-section .cmdline=cmdline.txt \
 --file-alignment 512 \
 --section-alignment 4096 \
 -O pei-x86-64 \
@@ -23,7 +24,3 @@ objcopy \
 BOOTX64.EFI
 
 rm ./bzImage
-
-#--add-section .cmdline=cmdline.txt \
-#--set-section-flags .cmdline=contents,alloc,load,readonly,data \
-#--change-section-vma .cmdline=0x2000000 \
