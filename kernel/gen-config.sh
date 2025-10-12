0c15a53295a07b67a9921bdf1bbdd22930722226
James Musselwhite <james.musselwhite@gmail.com>
2025-10-12 00:06:09 -0700

    just the one config

--- kernel/gen-config.sh
+++ kernel/gen-config.sh
@@ -7,15 +7,11 @@ build-essential \
 flex \
 bison
 
-cp storage.config source/
-cp cmdline.config source/
-cp framebuffer.config source/
+cp custom.config source/
 
 cd source
 
 make defconfig
-./scripts/kconfig/merge_config.sh .config storage.config cmdline.config framebuffer.config
+./scripts/kconfig/merge_config.sh .config custom.config
 cp .config ../config-6.17
-rm storage.config
-rm cmdline.config
-rm framebuffer.config
+rm custom.config
