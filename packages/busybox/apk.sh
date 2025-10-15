f7f94245dd8fe1669ac966ad7050da577f7d8c7a
James Musselwhite <james.musselwhite@gmail.com>
2025-10-14 22:26:10 -0700

    busybox working well

---
+++ packages/busybox/apk.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+apk update
+
+apk add \
+perl \
+musl-dev \
+gcc \
+make \
+linux-headers
--- packages/busybox/build.sh
+++ packages/busybox/build.sh
@@ -1,14 +1,5 @@
 #! /bin/sh
 
-apk update
-
-apk add \
-perl \
-musl-dev \
-gcc \
-make \
-linux-headers
-
 cd source
 make clean
 cp ../config-1.36.1 .config
