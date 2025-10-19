c50689960ab5d38f69ef26defe6b19b6b8929a75
James Musselwhite <james.musselwhite@gmail.com>
2025-10-19 14:29:42 -0700

    using virtio disk now

--- install/qemu.sh
+++ install/qemu.sh
@@ -8,5 +8,4 @@ qemu-system-x86_64 \
 -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/ovmf/OVMF_CODE.fd \
 -drive if=pflash,format=raw,file=./OVMF_VARS.fd \
 -drive file=disk.raw,format=raw,id=disk0,if=none \
--device ahci,id=ahci0 \
--device ide-hd,drive=disk0,bus=ahci0.0
+-device virtio-blk-pci,drive=disk0 # <--- PARAVIRTUALIZED DISK DEVICE
